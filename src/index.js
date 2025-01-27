784c1fc56e01920364217b20ec711f5f11bdf804
emremark <mark.radicc@gmail.com>
2025-01-27 22:00:28 +0100

    Add basic html layout and js module files for project and task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import "./styles.css";
+import "/home/vboxuser/Desktop/js/Todo-List/src/styles.css"
+import { projects } from "/home/vboxuser/Desktop/js/Todo-List/src/project"
+import { task } from "/home/vboxuser/Desktop/js/Todo-List/src/task"
---
+++ src/project.js
@@ -0,0 +1,24 @@
+export const projects = function () {
+    class Project {
+        constructor() {
+            this.task_list = [];
+        }
+
+    }
+}
+
+
+
+/*
+class Library {
+    constructor() {
+        this.books = [];
+    }
+    
+    addBook(title, author, pages, read) {
+        const book = {title, author, pages, read};
+        this.books.push(book);
+    }
+    
+}
+*/
